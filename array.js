65c2da1092ca8a8e8336c7636ca2a3b3f34a4753
pugazhenthi-sinivasan <pugalpugal631@gmail.com>
2024-09-19 07:41:08 +0530

    fill

--- array.js
+++ array.js
@@ -1,41 +1,41 @@
-// 1. Basic Array Creation  
-//    Create an array of 5 numbers and print the array.
-let cod=[ "for","whil","if","else","string"]
-console.log(cod);
+// // 1. Basic Array Creation  
+// //    Create an array of 5 numbers and print the array.
+// let cod=[ "for","whil","if","else","string"]
+// console.log(cod);
 
 
 
-let myf = ["apple", "banana", "cherry", "date"];// Print the 2nd and 4th elements
-console.log(myf[1]); // Output: banana
-console.log(myf[3]); // Output: date
-console.log(myf[0]);//  output:apple
+// let myf = ["apple", "banana", "cherry", "date"];// Print the 2nd and 4th elements
+// console.log(myf[1]); // Output: banana
+// console.log(myf[3]); // Output: date
+// console.log(myf[0]);//  output:apple
 
 
-//  3. Modifying Array Elements  
-//  Create an array of 3 elements. Change the 2nd element to "modified" and print the array.
-let Array1 = ["first", "second", "third"];// Change the 2nd element to "modified"
-Array1[1] = "modified";// Print the array
-console.log(Array1);
-
-
-
-// 4. Array Length  
-//    Write a program to print the length of an array containing 7 elements. 
-let blends=[ "brown","cron","drain","train",,"crab",]// array length 5
-console.log(blends.length)
+// //  3. Modifying Array Elements  
+// //  Create an array of 3 elements. Change the 2nd element to "modified" and print the array.
+// let Array1 = ["first", "second", "third"];// Change the 2nd element to "modified"
+// Array1[1] = "modified";// Print the array
+// console.log(Array1);
 
 
 
+// // 4. Array Length  
+// //    Write a program to print the length of an array containing 7 elements. 
+// let blends=[ "brown","cron","drain","train",,"crab",]// array length 5
+// console.log(blends.length)
 
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log("Reversed array:", array);
 
 
-let abc=["a","b","c","d"];
-for(i=abc.length; i>=0; i--){
-    console.log(abc[i]);
-}
+
+// let array = [1, 2, 3, 4, 5];
+// array.reverse();
+// console.log("Reversed array:", array);
+
+
+// let abc=["a","b","c","d"];
+// for(i=abc.length; i>=0; i--){
+//     console.log(abc[i]);
+// }
 
 
 let fruits=["apple","jakfort","banan","mengo","yuva"]
